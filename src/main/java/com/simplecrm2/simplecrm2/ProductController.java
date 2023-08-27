b2a52c4637932dbc71e2bfbf47f33ac94682328b
root <root@Starlight>
2023-08-27 16:43:37 +0800

    update assignment with logger

--- src/main/java/com/simplecrm2/simplecrm2/ProductController.java
+++ src/main/java/com/simplecrm2/simplecrm2/ProductController.java
@@ -2,19 +2,26 @@
 
 package com.simplecrm2.simplecrm2;
 
+// spring
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.ArrayList;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+// arraylist
+import java.util.ArrayList;
+
+// logger
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class ProductController {
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(ProductController.class);
 
     private ArrayList<Product> products = new ArrayList<>(); // create arraylist to store product objects
 
@@ -22,12 +29,14 @@ public class ProductController {
     @PostMapping("/products")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         products.add(product);
+        logger.info("🟢 Added 1 new product. Product ID: " + product.getId());
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
     // Read (GET ALL) with ResponseEntity
     @GetMapping("/products")
     public ResponseEntity<ArrayList<Product>> getAllProducts() {
+        logger.info("🟢 Viewed all product(s).");
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
@@ -36,9 +45,11 @@ public class ProductController {
     public ResponseEntity<Product> getProductById(@PathVariable String productId) {
         for (Product product : products) {
             if (product.getId().equals(productId)) {
+                logger.info("🟢 Viewed 1 product. Product ID: " + productId);
                 return new ResponseEntity<>(product, HttpStatus.OK);
             }
         }
+        logger.error("🔴 Incorrect product ID or product ID doesn't exist.");
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
